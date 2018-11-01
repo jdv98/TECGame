4a30d19ed03f4396401c4c3e887894a7713fa572
jdv98 <josevp8@hotmail.com>
2018-11-01 17:18:14 -0600

    Clases agregadas

--- TECGames/Program.cs
+++ TECGames/Program.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using TECGames.Diagram_classes;
+
 namespace TECGames
 {
     class Program
     {
+        List<Work> workList = new List<Work>();
+        List<Ubication> ubicationList = new List<Ubication>();
+        List<WorkSection> workSectionList = new List<WorkSection>();
+        List<Designer> designerList = new List<Designer>();
         static void Main(string[] args)
         {
-            Console.WriteLine("asda");
             Console.ReadKey();
         }
     }
