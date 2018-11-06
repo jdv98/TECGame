289adc09605807720a4607abdb2bb68c7f291372
jafetSuarez <jsuarez@estudiantec.cr>
2018-11-06 00:43:22 -0600

    Creación de metodo Courtship.

--- TECGames/Genetic Algorithm/GeneticAlgorithm.cs
+++ TECGames/Genetic Algorithm/GeneticAlgorithm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +9,45 @@ using TECGames.Diagram_classes;
 
 namespace TECGames.Genetic_Algorithm
 {
+
     class GeneticAlgorithm
     {
+        Dictionary<int, int> parents = new Dictionary<int, int>();
+
         List<Work> works = new List<Work>();
 
         List<Work> actualGeneration = new List<Work>();
-
+        
         public GeneticAlgorithm(List<Work> works)
         {
             this.works = works;
         }
 
-        public void Courtship() {
+        public void Courtship(List<Work> works) {
 
-            foreach(Work w in works)
+            works.Sort((a, b) => a.WorkSection.Price.CompareTo(b.WorkSection.Price));
+
+            foreach (Work w in works)
             {
                 for (int i = works.Count; i > 0; i--) {
-                    
 
+                    if (w.Id != works[i].Id && w.WorkSection.Price > works[i].WorkSection.Price) {
+
+                        parents.Add(w.Id, works[i].Id);
+                    }
                 }
             }
-
-
         }
 
+        public void Crossing()
+        {
+            foreach (KeyValuePair<int, int> w in parents)
+            {
+                //REPRODUCIR CREAR NUEVAS INSTANCIAS DE TRABAJO, ID? , HACER VERIFICACIONES DE HORARIOS.
+            }
+        }
+
+        
 
     }
 }
