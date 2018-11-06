655cca32f7c71e8db9ca76caf3e3debd4a0f7198
jafetSuarez <jsuarez@estudiantec.cr>
2018-11-05 21:22:21 -0600

    Genetic algorithm

---
+++ TECGames/Genetic Algorithm/GeneticAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TECGames.Diagram_classes;
+
+namespace TECGames.Genetic_Algorithm
+{
+    class GeneticAlgorithm
+    {
+        List<Work> works = new List<Work>();
+
+        List<Work> actualGeneration = new List<Work>();
+
+        public GeneticAlgorithm(List<Work> works)
+        {
+            this.works = works;
+        }
+
+        public void Courtship() {
+
+            foreach(Work w in works)
+            {
+                for (int i = works.Count; i > 0; i--) {
+                    
+
+                }
+            }
+
+
+        }
+
+
+    }
+}
